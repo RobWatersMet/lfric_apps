5a17b2f0abaa4a0f75707937e5b8bbe603e66f11
Rob Waters <rw796@cam.ac.uk>
2026-07-29 13:58:32 +0100

    added 3d chunking as a rose-stem option

--- applications/lfric_atm/build/psyclone_transmute_file_list.mk
+++ applications/lfric_atm/build/psyclone_transmute_file_list.mk
@@ -42,12 +42,15 @@ export PSYCLONE_PHYSICS_FILES = \
                                 sw_kernel_mod \
                                 ukca_aero_ctl \
                                 ukca_abdulrazzak_ghan \
-                                ukca_chemistry_ctl_full_mod \
-                                ukca_main1-ukca_main1 \
                                 sw_rad_tile_kernel_mod \
                                 jules_imp_kernel_mod \
                                 jules_exp_kernel_mod \
                                 jules_extra_kernel_mod
+
+# to be re-added back into psyclone list later
+# ukca_chemistry_ctl_full_mod
+# ukca_main1-ukca_main1
+
 
 ##### TRANSMUTE_INCLUDE_METHOD specify_include #####
 
