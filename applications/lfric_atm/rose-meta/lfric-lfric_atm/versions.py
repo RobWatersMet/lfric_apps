87d400428bc994fff33842beccad51f90335200a
Rob Waters <rw796@cam.ac.uk>
2026-08-21 09:56:30 +0100

    added macro output

--- applications/lfric_atm/rose-meta/lfric-lfric_atm/versions.py
+++ applications/lfric_atm/rose-meta/lfric-lfric_atm/versions.py
@@ -20,29 +20,28 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
 """
 
-class vn32_t593(MacroUpgrade):
 
+class vn32_t593(MacroUpgrade):
     """Upgrade macro for ticket #593 by Matthew Naylor."""
 
     BEFORE_TAG = "vn3.2"
     AFTER_TAG = "vn3.2_t593"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-lfric_atm
         """Upgrade a UM runtime app configuration."""
         nml = "namelist:chemistry"
         var = "ukca_chem_full_chunk_size"
         val = "-1,-1,-1"
         self.add_setting(config, [nml, var], val)
+
         return config, self.reports
